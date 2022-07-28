f74f8f83a25321b73a2452581f314d4c85b836ba
Senthil <senthil.jogi@gmail.com>
2022-07-28 22:23:54 +0200

    Add new version 2.0.7.2_SRK_3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Senthil 1, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Senthil 2, default config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -1124,8 +1124,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 140
-#define Y_BED_SIZE 100
+#define X_BED_SIZE 200 // 140
+#define Y_BED_SIZE 200 // 100
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
--- readmeSrk.md
+++ readmeSrk.md
@@ -1,5 +1,9 @@
 ## Version
 
+### 2.0.7.2_SRK_3
+1. Increase bed size to 200 200
+1.
+
 ### 2.0.7.2_SRK_1
 1. Add inbuild solid state relay for the heatbed
 1. Increase Heatbed temperature to 70 *C
